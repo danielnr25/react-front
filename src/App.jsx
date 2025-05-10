5b0660d2fa0a374fcc343bea9b531f279971ae6b
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2025-05-09 21:29:17 -0500

    feat:react-semana04

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <>
+        <div>
+            App
+        </div>
+        <Home />
+        <Login />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div>Bienvenido al Home</div>
+  )
+}
+
+export default Home
