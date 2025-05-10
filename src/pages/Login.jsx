c941467509f00d6542ad6acfb67fcfb121c4f5b6
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2025-05-09 22:52:30 -0500

    feat:semana04-react

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,46 @@
-
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const handleLogin = (e) =>{
+        e.preventDefault(); // previene el comportamiento por defecto del formulario
+        const data = { // crear objeto con los datos del usuario
+            username,
+            password
+        }
+        
+        fetch("http://localhost:3000/auth/login",{
+            method: "POST", 
+            headers: { 
+              "Content-Type": "application/json" // Tipo de contenido JSON
+            },
+            body: JSON.stringify(data) // Enviar los datos del usuario en formato JSON
+        }).then((response)=>{
+           if(!response.ok){
+                console.error("Error de la autenticación") // lanzar un error
+           }
+           return response.json();
+        }).then((result)=>{
+            console.log("Resultado de la autenticación", result);
+            if(result.data){
+                console.log('Todo exitoso....')
+                setError(null);
+                // me envia al dashboard
+            }else{
+                setError(result.message);
+            }
+        }).catch((error)=>{
+            console.error("Error: ",error);
+             setError(error.message);
+        }).finally(()=>{
+            console.log('finaliza la ejecucion')
+        })
+
+    }
+
   return (
       <div className="flex flex-1 mt-16 lg:mt-20 flex-col justify-center px-6 pt-20 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md shadow-md px-10 bg-gray-50 pb-10 pt-10 rounded-md">
@@ -9,18 +49,16 @@ const Login = () => {
             </h2>
        
        
-          <form action="#" method="POST" className="space-y-6">
+          <form onSubmit={handleLogin} method="POST" className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+              <label htmlFor="username" className="block text-sm/6 font-medium text-gray-900">
                Usuario
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
+                  type="text"
+                  
+                  onChange={(ev)=>setUsername(ev.target.value)}
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -38,7 +76,7 @@ const Login = () => {
                   name="password"
                   type="password"
                   required
-                  autoComplete="current-password"
+                  onChange={(ev)=>setPassword(ev.target.value)}
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
@@ -56,6 +94,8 @@ const Login = () => {
             <div className="text-center mt-4">
                 <p className="text-base text-gray-800">¿No tienes cuenta? <NavLink to="/register" className="text-blue-700 font-semibold">Regístrate</NavLink></p>
             </div>
+
+               {error && <p className="text-red-500 font-medium text-base">{error}</p>}
 
           </form>
 
